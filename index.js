21f5c2d2c913ef6ce7c7604d359f74c207d5c6dc
Alish Bista <bistace321@gmail.com>
2023-07-25 20:08:07 +0545

    mile1 completed

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const readline = require('readline');
+const readline = require('readline'); // Importing realine for input and output
+// Create a readline interface
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
+
+// Function to greet the user
+function greetUser() {
+    rl.question('Enter your name: ', (name)=>{
+        console.log(`Hello, ${name}! Welcome to the To-Do List App!`);
+        rl.close();
+    });
+}
+
+// Call the function to start the interaction
+greetUser();
