e11fc3919954f3c4fecdc7da38156b295b6cc72b
Alish Bista <bistace321@gmail.com>
2023-07-30 11:21:00 +0545

    Task Controller Created

--- src/controllers/TasksController.js
+++ src/controllers/TasksController.js
@@ -116,4 +116,74 @@ const deleteTask = async (id) => {
       throw new Error('Failed to delete task');
     }
   };
-module.exports = {deleteTask,addTask,getAllTasks,getTaskById,isTitleExist,updateTask,completeTask}
+
+  const AddTask = async (req,res) => {
+    try {
+      const {title,description} = req.body;
+      const alreadyExists = await isTitleExist(title);
+      if (alreadyExists){
+          return res.status(400).json({"Error":"Task Title Already Exists"})
+      }
+      const newTask = await addTask(title,description)
+      res.status(200).json(newTask);
+  } catch (error) {
+      console.error("Error adding task")
+      res.status(400).json({error})
+  }
+  }
+
+const GetTasks = async (req,res) => {
+  try {
+    const tasks = await getAllTasks();
+    if(tasks.length === 0 ){
+        return res.status(404).json({"Error":"Error fetching tasks"})
+    }
+    return res.status(200).json(tasks);
+} catch (error){
+    console.log(error);
+    return res.status(404).json({"message": " Error fetching tasks"})
+}
+}
+
+const getTaskByID = async (req,res) => {
+  try {
+    const taskId = req.params.id;
+    const task = await getTaskById(taskId);
+    res.status(200).json(task);
+}catch(error){
+    res.status(404).json({"error": "Task not found"});
+}
+}
+const UpdateTask = async (req,res) => {
+  try {
+    const taskId = parseInt(req.params.id);
+    const updatedData = req.body;
+    // res.status(200).json(updatedData);
+    const updatedTask = await updateTask(taskId,updatedData)
+    res.status(200).json(updatedTask);
+} catch (error) {
+    return res.status(500).json({"Error":"Task not found"})
+}
+}
+
+const DeleteTask = async (req,res) => {
+  try {
+    const taskId = parseInt(req.params.id);
+    const message = await deleteTask(taskId)
+    res.status(200).json({message:message});
+   } catch (error){
+    return res.status(500).json({"Error":"Failed to delete Task"})
+   }
+}
+
+const completedTask = async (req,res) => {
+      // res.status(200).json(req.params.id);
+      try {
+        const taskId = parseInt(req.params.id);
+        const completedTask = await completeTask(taskId)
+        res.status(200).json(completedTask)
+    } catch (err) {
+        return res.status(500).json({"Error":"Task not found"})
+    }
+}
+module.exports = {completedTask,DeleteTask,UpdateTask,getTaskByID,GetTasks,AddTask}
